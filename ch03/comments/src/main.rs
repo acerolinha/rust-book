036f659b161e5f1e6662d803a165c6e2d210d82b
Felipe Cardoso <agn0stkp@gmail.com>
2023-10-15 04:25:00 +0000

    feat(ch03): add comments

---
+++ ch03/comments/src/main.rs
@@ -0,0 +1,10 @@
+// simple comment
+
+// multiple line
+// comment
+
+fn main() { // comment at the end of a line
+
+    // This is a more common way of commenting a line
+    println!("Hello, world!");
+}
