d7fdeb8222be9ec9f1c4ab725b6fa4ad003864ee
Felipe Cardoso <agn0stkp@gmail.com>
2023-10-30 06:49:48 +0000

    feat(ch07): use keyword

--- ch07/restaurant/src/lib.rs
+++ ch07/restaurant/src/lib.rs
@@ -48,12 +48,59 @@ mod back_of_house {
     }
 }
 
+// using use keyword to create a shortcut to a path
+use crate::front_of_house::hosting;
+
+// creating a shortcut directly to the function
+// makes it unclear to know where is the source of it
+use crate::front_of_house::hosting::add_to_waitlist;
+
+// on the other hand, to bring structs, enums and other
+// full path is preferred
+use std::collections::HashMap; // this is an external package
+
+// bringing two paths with the same item name
+use std::fmt;
+use std::io;
+
+// parent is specified for disambiguation
+fn fun1(_: fmt::Result) {}
+fn fun2(_: io::Result<()>) {}
+
+// providing new names with as keyword
+
+use std::io::Result as IoResult;
+
+fn fun3(_: IoResult<()>) {}
+
+// re-exporting names with pub use
+pub use crate::back_of_house::Appetizer;
+
+// nested paths
+use std::{cmp::Ordering, i16};
+
+// using self
+use std::array::{self, IntoIter};
+
+// glob operator
+use std::collections::*;
+
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // by use keyword shortcut
+    hosting::add_to_waitlist();
+
+    // by unclear use keyword shortcut
+    add_to_waitlist();
+
+    // full path shortcut for struct
+    let mut map = HashMap::new();
+    map.insert(1, 2);
 
     let mut meal = back_of_house::Breakfast::summer("Rye");
     // The next line works because toast field is public
