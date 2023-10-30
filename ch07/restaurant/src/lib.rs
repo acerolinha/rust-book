776a91176f1c8e22291d9e84fee587b0ba1aac83
Felipe Cardoso <agn0stkp@gmail.com>
2023-10-30 06:33:42 +0000

    feat(ch07): add paths and public

--- ch07/restaurant/src/lib.rs
+++ ch07/restaurant/src/lib.rs
@@ -1,6 +1,7 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    // both mod and fn should be public to be accessible
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -12,4 +13,55 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+// using super to refer to parent module
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    // pub with structs
+    pub struct Breakfast {
+        pub toast: String,
+        season_fruit: String, // this field is private
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                season_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // making an enum public makes all its variants public
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // The next line works because toast field is public
+    meal.toast = String::from("Wheat");
+
+    // The next line won't compile because seasonal_fruit field is private
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
