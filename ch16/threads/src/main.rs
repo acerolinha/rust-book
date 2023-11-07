2389476f44782c1444ca7be37fd5816ff7445994
Felipe Cardoso <agn0stkp@gmail.com>
2023-11-07 10:27:39 +0000

    feat(ch16): add threads

---
+++ ch16/threads/src/main.rs
@@ -0,0 +1,33 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // creating a new thread
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    // uncommenting the next line will make the spawned thread
+    // to finish before proceeding
+    // handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // wait all threads to finish
+    handle.join().unwrap();
+
+    // using move closures
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+}
