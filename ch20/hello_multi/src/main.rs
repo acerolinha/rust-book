016291bd2fdcb3329e6dd68e09ce35b0dddc7403
Felipe Cardoso <agn0stkp@gmail.com>
2023-11-12 15:33:00 +0000

    feat(ch20): add multi threaded http server

---
+++ ch20/hello_multi/src/main.rs
@@ -0,0 +1,44 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+use hello_multi::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| handle_connection(stream));
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buff_reader = BufReader::new(&mut stream);
+    let request_line = buff_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.xhtml"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.xhtml")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.xhtml"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!(
+        "{status_line}\r\n\
+        Content-Length: {length}\r\n\r\n\
+        {contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
